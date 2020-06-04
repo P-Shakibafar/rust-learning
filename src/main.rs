96ac36ca4e3920845739acd777c1aa0277ab4877
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-04 12:22:05 +0430

    Lesson 22 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,17 @@
 fn main()
-{}
+{
+    let mut my_string = String::from("How is it going? My name is Hasta.");
+
+    // Length
+    println!("Length: {}", my_string.len());
+    // Is Empty>
+    println!("String is empty? {}", my_string.is_empty());
+
+    for token in my_string.split_whitespace() {
+        println!("{}", token);
+    }
+
+    println!("Does the string contain 'Hasta'? {}", my_string.contains("Hasta"));
+    my_string.push_str(" Welcome to your tutorial on Strings!");
+    println!("{}", my_string);
+}
