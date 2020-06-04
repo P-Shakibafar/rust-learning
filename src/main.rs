b6f444c6cc0e727eb9c4553ec903a3b44aff5a53
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-04 12:08:43 +0430

    Lesson 20 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
 fn main()
-{}
+{
+    // let numbers = [1, 2, 3, 4];
+    let numbers = [2; 20];
+
+    // for number in numbers.iter() {
+    //     println!("{}", number);
+    // }
+    for number in 0..numbers.len() {
+        println!("{}", numbers[number]);
+    }
+}
+
