6399779c70c32d8591ebac56335ec8e081865499
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-04 12:13:58 +0430

    Lesson 21 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,22 @@
+struct Rectangle {
+    with: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn print_desc(&self) {
+        println!("Rectangle: {} x {}", self.with, self.height);
+    }
+
+    fn is_square(&self) -> bool {
+        self.with == self.height
+    }
+}
+
 fn main()
-{}
+{
+    let my_rect = Rectangle { with: 5, height: 5 };
+
+    my_rect.print_desc();
+    println!("Rectangle is a square: {}", my_rect.is_square());
+}
