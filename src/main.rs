39bc69b5c6c2fb9ba1d87f30b7d0063b24f6b1e0
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-04 12:44:14 +0430

    Lesson 24 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
 fn main()
-{}
+{
+    // let my_vector: Vec<i32> = Vec::new();
+    let mut my_vector = vec![1, 2, 3, 4];
+
+    my_vector.push(34);
+    my_vector.remove(1);
+
+    for number in my_vector.iter() {
+        println!("{}", number);
+    }
+}
+
