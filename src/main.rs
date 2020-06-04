451a476f4f60fdd13bc5a42dea39c0d9cce3c541
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-04 12:55:59 +0430

    Lesson 26 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,12 @@
+use std::env;
+
 fn main()
-{}
+{
+    let args: Vec<String> = env::args().collect();
+
+    for arg in args.iter() {
+        println!("{}", arg);
+    }
+
+    // cargo run src/main.rs arg1 arg2
+}
