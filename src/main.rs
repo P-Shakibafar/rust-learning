7b3011e99d4695226db52e77cc42894a418d5e30
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-04 12:52:04 +0430

    Lesson 25 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,14 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main()
-{}
+{
+    let mut file = File::open("info.txt")
+        .expect("Can't open file!");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Oops! Can not read the file...");
+
+    println!("File Contents:\n\n{}", contents);
+}
