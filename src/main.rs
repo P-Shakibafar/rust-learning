bad8d41cacff8314409d527ce7c247227093e857
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-05 17:54:20 +0430

    Lesson 28 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,31 @@
+struct Person {
+    name: String,
+    age: u8,
+}
+
+trait HasVoiceBox {
+    fn speak(&self);
+    fn can_speak(&self) -> bool;
+}
+
+impl HasVoiceBox for Person {
+    fn speak(&self) {
+        println!("Hello, my name is {}", self.name);
+    }
+    fn can_speak(&self) -> bool {
+        if self.age > 0 {
+            return true;
+        }
+        return false;
+    }
+}
+
 fn main()
-{}
+{
+    let person = Person {
+        name: String::from("Hasta"),
+        age: 0,
+    };
+    person.speak();
+    println!("Can {} speak? {}", person.name, person.can_speak());
+}
