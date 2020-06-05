d5dfb43483d0b31b0389a383bc79a37878f34748
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-05 17:46:18 +0430

    Lesson 27 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,14 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main()
-{}
+{
+    let mut file = File::create("output.txt")
+        .expect("Could not create file!");
+
+    file.write_all(b"Welcome to dcode!")
+        .expect("Cannot write to the file, sorry mate.");
+
+}
+
+
