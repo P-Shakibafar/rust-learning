ffec9ce16e97e0ee17286dfe8770097c063a8f42
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-05 18:09:26 +0430

    Lesson 30 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,15 @@
+use std::io;
+
 fn main()
-{}
+{
+    let mut input = String::new();
+    println!("Hy mate! Say something:");
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("Success! You said: {}", input.to_uppercase());
+        }
+        Err(e) => println!("Oops! Something went wrong: {}", e),
+    }
+}
+
