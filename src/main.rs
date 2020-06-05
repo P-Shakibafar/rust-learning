179819f43ebf0d83c700213f8847ff8c6cf1908b
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-05 18:01:15 +0430

    Lesson 29 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,10 @@
 fn main()
-{}
+{
+    let number = 8;
+    match number {
+        1 => println!("It is one!"),
+        2..=9 => println!("It is greater then one!"),
+        10 | 11 => println!("It is either 10 or 11"),
+        _ => println!("It doesn't match!")
+    }
+}
