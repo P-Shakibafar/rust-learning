73d8e79c53041b4f6af65d2ea8ea70724e71a341
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-06 17:57:47 +0430

    Lesson 36 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,22 @@
+mod hasta {
+    fn chicken() {
+        println!("Chicken!");
+    }
+
+    pub fn print_message() {
+        chicken();
+        println!("How's it going!");
+    }
+
+    pub mod water {
+        pub fn print_message() {
+            println!("I'm water!");
+        }
+    }
+}
+
 fn main()
-{}
+{
+    hasta::print_message();
+    hasta::water::print_message();
+}
