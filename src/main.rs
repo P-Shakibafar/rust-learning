2693b2bd581e4b61bc9677dbd7e4015367f8d63e
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-06 18:08:48 +0430

    Lesson 37 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,24 @@
 fn main()
-{}
+{
+    let name = String::from("Hasta");
+    println!("Character at index 1:{}",
+             match name.chars().nth(1) {
+                 Some(c) => c.to_string(),
+                 None => "No character at index 8!".to_string()
+             }
+    );
+    println!("Occupation is {}",
+             match get_occupation(&name) {
+                 Some(o) => o,
+                 None => "No occupation found!"
+             }
+    );
+}
+
+fn get_occupation(name: &str) -> Option<&str> {
+    match name {
+        "Hasta" => Some("Web Dev"),
+        "Michael" => Some("Dentist"),
+        _ => None
+    }
+}
