966083a3bcc5a48d5a1ec8fbef6652001a899f56
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-06 17:20:40 +0430

    Lesson 32 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,14 @@
+extern crate rand;
+
+use rand::Rng;
+
 fn main()
-{}
+{
+    let random_number = rand::thread_rng()
+        .gen_range(1, 11);
+    println!("Random Number: {}", random_number);
+
+    let random_bool = rand::thread_rng()
+        .gen_bool(0.25);
+    println!("Random Boolean: {}", random_bool);
+}
