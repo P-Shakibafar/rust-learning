3c0136076be41fc8fc838285b4249e916438e590
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-06 19:03:19 +0430

    Lesson 39 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,26 @@
+#![allow(dead_code)]
+
+enum Day {
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat,
+    Sun,
+}
+
+impl Day {
+    fn is_weekday(&self) -> bool {
+        return match self {
+            &Day::Sat | &Day::Sun => false,
+            _ => true
+        };
+    }
+}
+
 fn main()
-{}
+{
+    let d = Day::Tue;
+    println!("Is d weekday? {}", d.is_weekday());
+}
