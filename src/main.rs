0af2d90231aa191169188ef4c9816d154351f6b0
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-06 17:52:04 +0430

    Lesson 35 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,17 @@
+extern crate regex;
+
+use regex::Regex;
+
 fn main()
-{}
+{
+    let re = Regex::new(r"(\w{5})").unwrap();
+    let text = "hasta";
+
+    match re.captures(text) {
+        // Some(caps) => println!("Found match: {}", caps.get(0).unwrap().as_str()),
+        Some(caps) => println!("Found match: {}", &caps[0]),
+        None => println!("Not found!")
+    }
+
+    println!("Found match? {}", re.is_match(text));
+}
