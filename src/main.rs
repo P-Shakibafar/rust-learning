36f6265ee785825489ccf4a6e99b37a3b2104765
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-08 21:07:11 +0430

    Lesson 40 done!

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
+use std::process::Command;
+
 fn main()
-{}
+{
+    let mut cmd = Command::new("python3.8");
+    cmd.arg("hasta.py");
+
+    match cmd.output() {
+        Ok(o) => {
+            unsafe {
+                println!("Output: {}", String::from_utf8_unchecked(o.stdout));
+            }
+        }
+        Err(e) => {
+            println!("There was an error! {}", e);
+        }
+    }
+}
